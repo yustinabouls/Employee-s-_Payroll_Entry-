7dc33eacd4bddba2b289466a20b783d7a370bc4b
Yustina Bouls <101233246+yustinabouls@users.noreply.github.com>
2023-06-24 11:32:43 -0400

    Add files via upload

---
+++ Lab-3/Data.cs
@@ -0,0 +1,221 @@
+﻿// DataAccess.cs
+//         Title: DataAccess - Data Access Layer for Piecework Payroll
+// Last Modified: 
+//    Written By: Yustina Bouls
+// Based on code samples provided by Kyle Chapman
+// 
+// This is a module with a set of classes allowing for interaction between
+// Piecework Worker data objects and a database.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Lab_3
+{
+    class DataAccess
+    {
+
+        #region "Connection String"
+
+        /// <summary>
+        /// Return connection string
+        /// </summary>
+        /// <returns>Connection string</returns>
+        private static string GetConnectionString()
+        {
+            /* Somehow, we need to have a working connection string.
+               This can be a local connecting string, like mine below,
+               but you can't expect this to work on your machine (or
+               anyone else's). If you use this, I will attempt to
+               figure it out during grading.
+               Other options may be available; you may want to experiment
+               with remote databases but there are reasons I can't make
+               this a requirement. */
+
+            return "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" +
+                Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()).ToString()).ToString() +
+                "\\WorkerDatabase.mdf;Integrated Security=True";
+
+            // The following is better - no absolute/fixed path - but it won't actually work while we're debugging. You can try a variant on this.
+            //return "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" +
+            //    Directory.GetCurrentDirectory() +
+            //    "\\WorkerDatabase.mdf;Integrated Security=True;Connect Timeout=30";
+
+        }
+
+        #endregion
+
+        #region "Methods"
+
+        /// <summary>
+        /// Function that returns all workers in the database as a DataTable for display
+        /// </summary>
+        /// <returns>a DataTable containing all workers in the database</returns>
+        internal static DataTable GetEmployeeList()
+        {
+            // Declare the SQL connection, SQL command, and SQL adapter
+            SqlConnection dbConnection = new SqlConnection(GetConnectionString());
+            SqlCommand command = new SqlCommand("SELECT * FROM [tblEntries]", dbConnection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            // Declare a DataTable object that will hold the return value
+            DataTable employeeTable = new DataTable();
+
+            // Try to connect to the database, and use the adapter to fill the table
+            try
+            {
+                dbConnection.Open();
+                adapter.Fill(employeeTable);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show("A database error has been encountered: " + Environment.NewLine + ex.Message, "Database Error");
+            }
+            finally
+            {
+                adapter.Dispose();
+                dbConnection.Close();
+            }
+
+            // Return the populated DataTable
+            return employeeTable;
+        }
+
+        /// <summary>
+        /// Function to add a new worker to the worker database
+        /// </summary>
+        /// <param name="insertWorker">a worker object to be inserted</param>
+        /// <returns>true if successful</returns>
+        internal static bool InsertNewRecord(PieceworkWorker insertWorker)
+        {
+            // Create return value
+            bool returnValue = false;
+
+            // Declare the SQL connection
+            SqlConnection dbConnection = new SqlConnection(GetConnectionString());
+
+            // Create new SQL command and assign it paramaters
+            SqlCommand command = new SqlCommand("INSERT INTO tblEntries VALUES(@firstName, @lastName, @messages, @pay, @entryDate)", dbConnection);
+            command.Parameters.AddWithValue("@firstName", insertWorker.FirstName);
+            command.Parameters.AddWithValue("@lastName", insertWorker.LastName);
+            command.Parameters.AddWithValue("@messages", insertWorker.Messages);
+            command.Parameters.AddWithValue("@pay", insertWorker.Pay);
+            command.Parameters.AddWithValue("@entryDate", insertWorker.EntryDate);
+
+            // The above SQL command is the same as the following:
+            // SqlCommand command = new SqlCommand("INSERT INTO tblEntries VALUES(" + insertWorker.FirstName + ", " + insertWorker.LastName + ", " + insertWorker.Messages + ", " + insertWorker.Pay + ", " + insertWorker.EntryDate + ")", dbConnection);
+            // Your choice if you think this version is nicer!
+
+            // Try to insert the new record, return result
+            try
+            {
+                dbConnection.Open();
+                returnValue = (command.ExecuteNonQuery() == 1);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show("A database error has been encountered: " + Environment.NewLine + ex.Message, "Database Error");
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+
+            // Return the true if this worked, false if it failed
+            return returnValue;
+        }
+
+        /// <summary>
+        /// Returns a total number of employees from the database.
+        /// </summary>
+        /// <returns>total employees, as a string</returns>
+        internal static string GetTotalEmployees()
+        {
+            // Declare the SQL connection and the SQL command
+            SqlConnection dbConnection = new SqlConnection(GetConnectionString());
+            SqlCommand command = new SqlCommand("SELECT COUNT(Id) FROM tblEntries", dbConnection);
+
+            // Try to open a connection to the database and read the total. Return result.
+            try
+            {
+                dbConnection.Open();
+                return command.ExecuteScalar().ToString();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show("A database error has been encountered: " + Environment.NewLine + ex.Message, "Database Error");
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+            // If something unexpected occured, set the result as error
+            return "ERROR";
+        }
+
+        /// <summary>
+        /// Returns a total number of messages from the database.
+        /// </summary>
+        /// <returns>total messages, as a string</returns>
+        internal static string GetTotalMessages()
+        {
+            // Declare the SQL connection and the SQL command
+            SqlConnection dbConnection = new SqlConnection(GetConnectionString());
+            SqlCommand command = new SqlCommand("SELECT SUM(Messages) FROM tblEntries", dbConnection);
+
+            // Try to open a connection to the database and read the total. Return result.
+            try
+            {
+                dbConnection.Open();
+                return command.ExecuteScalar().ToString();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show("A database error has been encountered: " + Environment.NewLine + ex.Message, "Database Error");
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+            // If something unexpected occured, set the result as error
+            return "ERROR";
+        }
+
+        /// <summary>
+        /// Returns a total pay among all employees from the database.
+        /// </summary>
+        /// <returns>total pay, as a string</returns>
+        internal static string GetTotalPay()
+        {
+            // Declare the SQL connection and the SQL command
+            SqlConnection dbConnection = new SqlConnection(GetConnectionString());
+            SqlCommand command = new SqlCommand("SELECT SUM(Pay) FROM tblEntries", dbConnection);
+
+            // Try to open a connection to the database and read the total. Return result.
+            try
+            {
+                dbConnection.Open();
+                return command.ExecuteScalar().ToString();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show("A database error has been encountered: " + Environment.NewLine + ex.Message, "Database Error");
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+            // If something unexpected occured, set the result as error
+            return "ERROR";
+        }
+
+        #endregion
+
+    }
+}
---
+++ Lab-3/PieceworkWorker.cs
@@ -0,0 +1,274 @@
+﻿// PieceworkWorker.cs
+//         Title: IncInc Payroll (Piecework)
+// Last Modified: November 1, 2020
+//    Written By: Yustina Bouls
+// Adapted from PieceworkWorker by Kyle Chapman, September 2019
+// 
+// This is a class representing individual worker objects. Each stores
+// their own name and number of messages and the class methods allow for
+// calculation of the worker's pay and for updating of shared summary
+// values. Name and messages are received as strings.
+// This is being used as part of a piecework payroll application.
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lab_3
+{
+    class PieceworkWorker
+    {
+
+        #region "Variable declarations"
+
+        // Instance variables
+        private string WorkerFirstName;
+        private string WorkerLastName;
+        private int employeeMessages;
+        private decimal employeePay;
+
+        // Shared summary value variables
+        private static int totalEmployees = 0;
+        private static int totalMessages = 0;
+        private static decimal totalPay = 0M;
+
+        #endregion
+
+        #region "Constructors"
+
+        /// <summary>
+        /// PieceworkWorker constructor: accepts a worker's name and number of
+        /// messages, sets and calculates values as appropriate.
+        /// </summary>
+        /// <param name="nameValue">the worker's name</param>
+        /// <param name="messageValue">a worker's number of messages sent</param>
+        public PieceworkWorker(string firstNameValue, string lastNameValue, string messagesValue)
+        {
+            // Validate and set the worker's name
+            this.WorkerFirstName = firstNameValue;
+            // Validate and set the worker's last name
+            this.WorkerLastName = lastNameValue;
+            // Validate Validate and set the worker's number of messages
+            this.Messages = messagesValue;
+            // Calculcate the worker's pay and update all summary values
+            this.EntryDate = DateTime.Now;
+            FindPay();
+
+            DataAccess.InsertNewRecord(this);
+
+        }
+
+        /// <summary>
+        /// PieceworkWorker constructor: empty constructor used strictly for inheritance and instantiation
+        /// </summary>
+        public PieceworkWorker()
+        {
+
+        }
+
+
+
+        #endregion
+
+        #region "Class methods"
+
+        /// <summary>
+        /// Currently called in the constructor, the findPay() method is
+        /// used to calculate a worker's pay using threshold values to
+        /// change how much a worker is paid per message. This also updates
+        /// all summary values.
+        /// </summary>
+        private void FindPay()
+        {
+
+            // Declare a large bank of constants describing the pay structure.
+            const int LowestMessageThreshold = 1000;
+            const int LowMessageThreshold = 2000;
+            const int MediumMessageThreshold = 3000;
+            const int HighMessageThreshold = 4000;
+
+            // Declare a large bank of constants for the various pay rates.
+            const decimal LowestPayRate = 0.021M;
+            const decimal LowPayRate = 0.028M;
+            const decimal MediumPayRate = 0.035M;
+            const decimal HighPayRate = 0.040M;
+            const decimal HighestPayRate = 0.045M;
+
+            // Identify the worker's payment level and calculate their pay.
+            if (employeeMessages < LowestMessageThreshold)
+            {
+                Pay = employeeMessages * LowestPayRate;
+            }
+            else if (employeeMessages < LowMessageThreshold)
+            {
+                Pay = employeeMessages * LowPayRate;
+            }
+            else if (employeeMessages < MediumMessageThreshold)
+            {
+                Pay = employeeMessages * MediumPayRate;
+            }
+            else if (employeeMessages < HighMessageThreshold)
+            {
+                Pay = employeeMessages * HighPayRate;
+            }
+            else
+            {
+                Pay = employeeMessages * HighestPayRate;
+            }
+
+            // Update all shared summary values.
+            TotalPay += employeePay;
+           TotalMessages += employeeMessages;
+        }
+            TotalWorkers++;
+
+        #endregion
+
+        #region "Property Procedures"
+
+        /// <summary>
+        /// Gets and sets a worker's name
+        /// </summary>
+        /// <returns>an employee's name</returns>
+        internal string FirstName
+        {
+            get
+            {
+                return WorkerFirstName;
+            }
+            set
+            {
+                // Check if the value is empty.
+                if (value.Trim() != String.Empty)
+                {
+                    // Set the value.
+                    WorkerFirstName = value;
+                }
+                else
+                {
+                    // Value was empty; report as an error.
+                    throw new ArgumentNullException("Name", "You must enter a name to proceed.");
+                }
+            }
+        }
+        internal string LastName
+        {
+            get
+            {
+                return WorkerLastName;
+            }
+            set
+            {
+                // Check if the value is empty.
+                if (value.Trim() != String.Empty)
+                {
+                    // Set the value.
+                    WorkerLastName = value;
+                }
+                else
+                {
+                    // Value was empty; report as an error.
+                    throw new ArgumentNullException("Name", "You must enter a firstname to proceed.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets and sets the number of messages sent by a worker
+        /// </summary>
+        /// <returns>an employee's number of messages</returns>
+        internal string Messages
+        {
+            get
+            {
+                return employeeMessages.ToString();
+            }
+            set
+            {
+                const int MinimumMessages = 1;
+                const int MaximumMessages = 10000;
+
+                // Check if the value is an integer, and attempt to store it.
+                if (!int.TryParse(value, out employeeMessages))
+                {
+                    // Value was not a whole number; report as an error.
+                    throw new ArgumentException("You must enter the messages as a whole number.", "Messages");
+                }
+                // If the stored value is not a positive number, report an error.
+                else if (employeeMessages < MinimumMessages || employeeMessages > MaximumMessages)
+                {
+                    throw new ArgumentOutOfRangeException("Messages", "Messages must be entered between " + MinimumMessages + " and " + MaximumMessages + ".");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the worker's pay
+        /// </summary>
+        /// <returns>a worker's pay</returns>
+        internal decimal Pay { get; set; }
+        /// <summary>
+        /// Gets the overall pay for all workers
+        /// </summary>
+        /// <returns>the overall pay total for all workers</returns>
+
+        internal static decimal TotalPay { get; private set; } = 0M;
+
+        /// <summary>
+        /// Gets the overall number of workers
+        /// </summary>
+        /// <returns>the overall number of workers</returns>
+        internal static int TotalWorkers { get; private set; } = 0;
+
+
+        /// <summary>
+        /// Gets the overall number of messages sent
+        /// </summary>
+        /// <returns>the overall number of messages sent</returns>
+        internal static int TotalMessages { get; private set; } = 0;
+       
+
+        /// <summary>
+        /// Calculates and returns an average pay among all workers
+        /// </summary>
+        /// <returns>the average pay among all workers</returns>
+        internal static decimal AveragePay
+        {
+            get
+            {
+                if (TotalWorkers == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return TotalPay / TotalWorkers;
+                }
+            }
+        }
+
+        internal DateTime EntryDate { get; set; }
+
+
+        /// <summary>
+        /// Gets the pay for the workers
+        /// </summary>
+        /// <returns>workers pay</returns>
+        /// 
+
+        internal static DataTable AllWorkers
+        {
+            get
+            {
+                return DataAccess.GetEmployeeList();
+            }
+        }
+
+        #endregion
+    }
+}
+
